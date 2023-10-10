e5decdcfa0f33293ac8dbd7da509e9c23a395280
adung1703 <adung1703@gmail.com>
2023-10-10 21:43:26 +0700

    Image

--- app/src/main/java/com/huudung/diceroller/MainActivity.kt
+++ app/src/main/java/com/huudung/diceroller/MainActivity.kt
@@ -3,10 +3,13 @@ package com.huudung.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,11 +17,28 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        rollDice()
     }
 
+
     private fun rollDice() { //Update value of id.textView
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = Dice(6).roll().toString()
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Update the ImageView with the correct drawable resource ID
+        val drawableResource = when(diceRoll) {
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
